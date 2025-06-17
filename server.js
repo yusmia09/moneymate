865799f249990a7fbb9fae3224f087e23ac4794f
Maudiadell <maudyadella273@gmail.com>
2025-06-18 02:47:53 +0700

    deploy railway5

--- server.js
+++ server.js
@@ -8,21 +8,21 @@ const User = require("./models/User");
 
 const app = express();
 
-app.use(cors()); // Mengaktifkan CORS
-app.use(express.json()); // Menghubungkan Express dengan body-JSON
+app.use(cors()); 
+app.use(express.json());
 
-// Cek koneksi ke database sebelum starting server
+
 sequelize.authenticate()
   .then(() => console.log("Database connected!"))
   .then(() => {
-    // Sinkronisasi models -> bikin table sesuai model yang Adel punya
+    
     return sequelize.sync();
   })
   .then(() => console.log("Table siap!"))
   .catch((err) => console.error("Unable to connect to db!", err));
 
 
-// Import routes
+
 const authRoutes = require("./routes/auth");
 
 const walletRoutes = require("./routes/wallet");
@@ -34,7 +34,7 @@ const profileRoutes = require("./routes/profile");
 const statisticRoutes = require("./routes/statistic");
 
 
-// Menghubungkan routes
+
 app.use('/api/auth', authRoutes);
 app.use('/api/wallets', walletRoutes);
 app.use('/api/transactions', transactionRoutes);
@@ -42,12 +42,12 @@ app.use('/api/profile', profileRoutes);
 app.use('/api', statisticRoutes);
 
 
-// Port dari env
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server berjalan di port ${port}`));
 
 
-// Jika memang Adel deploy di serverless (railway serverless)
+
 module.exports = app;
 module.exports.handler = serverless(app);
