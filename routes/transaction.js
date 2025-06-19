e729577f2e89a95bf04f18bafebbe9ef16d37e32
Maudiadell <maudyadella273@gmail.com>
2025-06-20 05:45:43 +0700

    apa

--- routes/transaction.js
+++ routes/transaction.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const transactionController = require('../controllers/transactionController');
 
 router.post('/',transactionController.createTransaction);
+router.get('/', transactionController.getTransactionsByUser); 
 router.get('/:walletId', transactionController.getTransactionsByWallet);
 router.put('/:id', transactionController.updateTransaction);
 router.delete('/:id', transactionController.deleteTransaction);
-router.get('/', transactionController.getTransactionsByUser); 
+
 
 module.exports = router;
