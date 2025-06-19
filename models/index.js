dd375a48c53a28aed7d3bfa34aa5b53a259b85af
Maudiadell <maudyadella273@gmail.com>
2025-06-20 05:50:56 +0700

    bisa

--- models/index.js
+++ models/index.js
@@ -1,13 +1,20 @@
-const User = require('./User');
-const Wallet = require('./Wallet');
-const Transaction = require('./Transaction');
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../config/db'); 
+
+const User = require('./User')(sequelize, DataTypes);
+const Wallet = require('./Wallet')(sequelize, DataTypes);
+const Transaction = require('./Transaction')(sequelize, DataTypes);
 
 
 User.hasMany(Wallet, { foreignKey: 'userId' });
 Wallet.belongsTo(User, { foreignKey: 'userId' });
 
-
 Wallet.hasMany(Transaction, { foreignKey: 'walletId', as: 'transactions' });
 Transaction.belongsTo(Wallet, { foreignKey: 'walletId', as: 'wallet' });
 
-module.exports = { User, Wallet, Transaction };
+module.exports = {
+  sequelize,
+  User,
+  Wallet,
+  Transaction
+};
